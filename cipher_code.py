193cf15c813d9c440c5bcb3edf2ff2e9cabd61f8
drogtke958 <153301526+drogtke958@users.noreply.github.com>
2024-10-04 10:29:59 -0400

    Update cipher_code.py

--- cipher_code.py
+++ cipher_code.py
@@ -1,3 +1,47 @@
 # Keily Drogt
 # 4 Oct 2024
 # Cipher Code
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+new_message = ""
+
+user_message = input("Enter your secret message: ")
+key = int(input("Enter a key: "))
+#print(user_message)
+
+for character in user_message:
+  if character is alphabet:
+    position = alphabet.find(character)
+    new_position = (position + key) % 26
+    new_character = alphabet[new_position]
+    new_message += new_character
+print("Your new message is " + new_message)
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+new_message = ""
+
+user_message = input("Enter your secret message: ").lower()
+key = int(input("Enter a key: "))
+#print(user_message)
+
+for character in user_message:
+  if character is alphabet:
+    position = alphabet.find(character)
+    new_position = (position + key) % 26
+    new_character = alphabet[new_position]
+    new_message += new_character
+
+  else:
+    new_message += character
+print("Your new message is " + new_message)
+
+
+
+
+
+
+
+
+
+
+
